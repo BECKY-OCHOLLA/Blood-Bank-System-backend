820515f3731d8d5be8b876e5c8761845eabde160
Langat20 <kipkuruie7.lang@gmail.com>
2022-07-05 20:42:55 +0300

    ft: add get request

--- blood/views.py
+++ blood/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Stock,BloodRequest
+from .serializers import BloodRequestSerializer, StockSerializer
+from django.shortcuts import render
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.parsers import JSONParser 
+
+
+
+class StockList(APIView):
+      
+      permission_classes = (AllowAny,)
+      serializer_class = StockSerializer
+    # To get all stock
+      def get(self, request, format=None):
+          all_stock = Stock.objects.all()
+          serializers = StockSerializer(all_stock, many=True)
+          return Response(serializers.data)
+    
+    
+      
+
+    
+    
+    
+    
+    
