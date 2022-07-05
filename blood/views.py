5fa2a95c74522a024f99f2ca313aa7ada17244a8
Langat20 <kipkuruie7.lang@gmail.com>
2022-07-05 20:45:16 +0300

    ft: add put request to add update to Stocklist

--- blood/views.py
+++ blood/views.py
@@ -29,8 +29,15 @@ class StockList(APIView):
               return Response(serializers.data, status=status.HTTP_201_CREATED)
           return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-         
-
+    # To update stock
+      def put(self, request, format=None):
+        serializer = StockSerializer(data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+   
     
     
     
